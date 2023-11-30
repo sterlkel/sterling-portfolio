394142700fef8d07802ae4ce90a2aa4134bc041e
Sterling Kelly <sterlkel@gmail.com>
2023-11-29 21:28:45 -0500

    Adds email sent screen using cookie

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,7 +1,13 @@
 import { Card, CardContent, CardHeader, Container } from "@mui/material";
 import ContactForm from "./ContactForm";
+import { useEffect, useState } from "react";
+import { getEmailSent } from "@/app/actions";
 
 const Contact = () => {
+  const [emailSent, setEmailSent] = useState(false);
+  useEffect(() => {
+    getEmailSent().then((date) => setEmailSent(date));
+  }, []);
   return (
     <div className="h-screen d-flex" id="contact">
       <Container className="mt-2 justify-content-center d-flex flex-column">
@@ -11,8 +17,14 @@ const Contact = () => {
             title="Get In Touch"
             subheader="Contact me!"
           />
-          <CardContent>
-            <ContactForm />
+          <CardContent className="d-flex">
+            {!emailSent ? (
+              <ContactForm />
+            ) : (
+              <div className="justify-content-center">
+                <h1>Email Sent</h1>
+              </div>
+            )}
           </CardContent>
         </Card>
       </Container>
