da4a1fb89bf9836e348c63f06af7563db9822493
Sterling Kelly <sterlkel@gmail.com>
2023-12-15 13:28:37 -0500

    enlarges resume icon

--- src/components/Resume/Experience/Experience.tsx
+++ src/components/Resume/Experience/Experience.tsx
@@ -1,13 +1,10 @@
 "use client";
 
-import {
-  VerticalTimeline,
-  VerticalTimelineElement,
-} from "react-vertical-timeline-component";
-import Image from "next/image";
+import { VerticalTimeline } from "react-vertical-timeline-component";
 import { experiences } from "@/constants";
 import ExperienceCard from "./ExperienceCard";
-import { Button, Container } from "@mui/material";
+import { Container } from "@mui/material";
+import ResumeCardTimelineElement from "./ResumeCardTimelineElement";
 
 const Experience = () => {
   return (
@@ -16,30 +13,7 @@ const Experience = () => {
         {experiences.map((experience, index) => (
           <ExperienceCard key={index} experience={experience} />
         ))}
-        <VerticalTimelineElement
-          contentStyle={{
-            background: "#eaeaec",
-            color: "#292929",
-          }}
-          contentArrowStyle={{
-            borderRight: "7px solid  #232631",
-          }}
-          icon={<Image src="/resume.png" height={50} width={50} alt="resume" />}
-        >
-          <Button
-            onClick={() =>
-              window.open(
-                "resume link", //paste the link to resume here
-                "_blank"
-              )
-            }
-            variant="contained"
-            className="text-primary font-mulish"
-          >
-            MY RESUME
-            <Image src="/download.png" alt="download" height={25} width={25} />
-          </Button>
-        </VerticalTimelineElement>
+        <ResumeCardTimelineElement />
       </VerticalTimeline>
     </Container>
   );
